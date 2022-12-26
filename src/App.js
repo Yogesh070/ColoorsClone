4220253ad3585451421dd9d8651b427eaa253e1e
ythapa430 <ythapa430@gmail.com>
2022-12-27 00:03:01 +0545

    [update] react v18

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
-import {React,useState,useEffect} from 'react'
+import React,{ useState, useEffect } from 'react'
 import ColorPalette from './ColorPalette';
 import './App.css'
 
 function App() {
-    const [colorPalette, setColorPalette] = useState([]);
-    useEffect(() => {
-      randomColorGenerator();
-      window.addEventListener("keydown", (e)=>{
-        if (e.key===' ') {
-          randomColorGenerator();
-        }
-        if ((e.metaKey || e.ctrlKey) && e.code === 'KeyX') {
-          console.log('fire!')
-      }  
-      });
-    }, []);
+  const [colorPalette, setColorPalette] = useState([]);
+  useEffect(() => {
+    randomColorGenerator();
+    window.addEventListener("keydown", (e) => {
+      if (e.key === ' ') {
+        randomColorGenerator();
+      }
+      if ((e.metaKey || e.ctrlKey) && e.code === 'KeyX') {
+        console.log('fire!')
+      }
+    });
+  }, []);
 
-    const randomColorGenerator=()=>{
-        let tempArr=[];
-        for (let i = 0; i < 5; i++) {
-            let randomColor = Math.floor(Math.random()*14468846).toString(16);
-            const element = randomColor;
-            tempArr.push(element);
-        }
-        setColorPalette(tempArr);
+  const randomColorGenerator = () => {
+    let tempArr = [];
+    for (let i = 0; i < 5; i++) {
+      let randomColor = Math.floor(Math.random() * 14468846).toString(16);
+      const element = randomColor;
+      tempArr.push(element);
     }
-    return (
-        <div className="main">
-        {colorPalette.map((color,index)=><ColorPalette color={color} key={index} i={index}/>)}
-        </div>
-    )
+    setColorPalette(tempArr);
+  }
+  return (
+    <div className="main">
+      {colorPalette.map((color, index) => <ColorPalette color={color} key={index} i={index} />)}
+    </div>
+  )
 }
 
 export default App
-
--- src/ColorPalette.js
+++ src/ColorPalette.js
@@ -1,25 +1,25 @@
-import {React,useState} from 'react'
+import React, { useState } from 'react'
 import './App.css'
-import {MdClose} from 'react-icons/md'
-import {FiCopy,FiLock,FiStar} from 'react-icons/fi'
-import {BsGrid} from 'react-icons/bs'
+import { MdClose } from 'react-icons/md'
+import { FiCopy, FiLock, FiStar } from 'react-icons/fi'
+import { BsGrid } from 'react-icons/bs'
 
 
-function ColorPalette({color,i}) {
+function ColorPalette({ color, i }) {
     const [isShown, setIsShown] = useState(false);
     const [showShades, setshowShades] = useState(false);
-    const actions=[
-        {icon:<MdClose size={28}/>,tooltipText:"Remove Color"},
-        {icon:<BsGrid size={24}/>,tooltipText:"Show Shades"},
-        {icon:<FiCopy size={24}/>,tooltipText:"Copy HEX"},
-        {icon:<FiStar size={24}/>,tooltipText:"Add to Favourite"},
-        {icon:<FiLock size={24}/>,tooltipText:"Lock Color"}
+    const actions = [
+        { icon: <MdClose size={28} />, tooltipText: "Remove Color" },
+        { icon: <BsGrid size={24} />, tooltipText: "Show Shades" },
+        { icon: <FiCopy size={24} />, tooltipText: "Copy HEX" },
+        { icon: <FiStar size={24} />, tooltipText: "Add to Favourite" },
+        { icon: <FiLock size={24} />, tooltipText: "Lock Color" }
     ];
     function copyToClipBoard() {
         navigator.clipboard.writeText('hi');
     }
     function toggleShades() {
-        setshowShades(prev=>!prev);
+        setshowShades(prev => !prev);
     }
     function performAction(i) {
         switch (i) {
@@ -43,24 +43,24 @@ function ColorPalette({color,i}) {
                 break;
         }
     }
-    function ColorPaletteChild({color}) {
-        let arr=[];
+    function ColorPaletteChild({ color }) {
+        let arr = [];
         for (let i = 0; i < 20; i++) {
-            arr.push(<div className="palette-child" style={{backgroundColor:`#${color}`,opacity:i*0.06 , display: `${showShades?"block":"none"}`}} key={i}></div>);
+            arr.push(<div className="palette-child" style={{ backgroundColor: `#${color}`, opacity: i * 0.06, display: `${showShades ? "block" : "none"}` }} key={i}></div>);
         }
         return arr
     }
     return (
-        <div className="palette" style={{backgroundColor:`${!showShades? `#${color}` : "transparent"}`}}  onMouseEnter={() => setIsShown(true)}
-        onMouseLeave={() => setIsShown(false)}>
-            {isShown && ( <div className="toolbars" >
+        <div className="palette" style={{ backgroundColor: `${!showShades ? `#${color}` : "transparent"}` }} onMouseEnter={() => setIsShown(true)}
+            onMouseLeave={() => setIsShown(false)}>
+            {isShown && (<div className="toolbars" >
                 <ul>
-                    {actions.map((action,index)=><li key={index} onClick={()=>performAction(index)}  className="tooltip">{action.icon}<span className="tooltiptext">{action.tooltipText}</span></li>)}
+                    {actions.map((action, index) => <li key={index} onClick={() => performAction(index)} className="tooltip">{action.icon}<span className="tooltiptext">{action.tooltipText}</span></li>)}
                 </ul>
             </div>)
             }
-            <ColorPaletteChild color={color}/>
-         <h2>{color.toUpperCase()}</h2>
+            <ColorPaletteChild color={color} />
+            <h2>{color.toUpperCase()}</h2>
         </div>
     )
 }
